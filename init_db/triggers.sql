5cb666a97c784707a7485947662caca5db562616
Lam Devops <lamdt@icsp.vn>
2025-03-10 16:06:56 +0700

    add trigger db

---
+++ init_db/triggers.sql
@@ -0,0 +1,47 @@
+-- Trigger cho NAVIGATION_MENU_POSITION (trước khi INSERT)
+DELIMITER $$
+CREATE TRIGGER enforce_navbar_horizontal_before_insert 
+BEFORE INSERT ON setting
+FOR EACH ROW
+BEGIN
+  IF NEW.setting = 'NAVIGATION_MENU_POSITION' THEN
+    SET NEW.value = 'horizontal';
+  END IF;
+END$$
+DELIMITER ;
+
+-- Trigger cho NAVIGATION_MENU_POSITION (trước khi UPDATE)
+DELIMITER $$
+CREATE TRIGGER enforce_navbar_horizontal_before_update 
+BEFORE UPDATE ON setting
+FOR EACH ROW
+BEGIN
+  IF NEW.setting = 'NAVIGATION_MENU_POSITION' THEN
+    SET NEW.value = 'horizontal';
+  END IF;
+END$$
+DELIMITER ;
+
+-- Trigger cho GLOBAL_THEME_NAME (trước khi INSERT)
+DELIMITER $$
+CREATE TRIGGER enforce_global_theme_default_before_insert 
+BEFORE INSERT ON setting
+FOR EACH ROW
+BEGIN
+  IF NEW.setting = 'GLOBAL_THEME_NAME' THEN
+    SET NEW.value = 'default';
+  END IF;
+END$$
+DELIMITER ;
+
+-- Trigger cho GLOBAL_THEME_NAME (trước khi UPDATE)
+DELIMITER $$
+CREATE TRIGGER enforce_global_theme_default_before_update 
+BEFORE UPDATE ON setting
+FOR EACH ROW
+BEGIN
+  IF NEW.setting = 'GLOBAL_THEME_NAME' THEN
+    SET NEW.value = 'default';
+  END IF;
+END$$
+DELIMITER ;
