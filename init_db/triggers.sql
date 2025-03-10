451442cd4bcca3bdaa87f285e327cce9d1cf18b4
Lam Devops <lamdt@icsp.vn>
2025-03-10 16:25:36 +0700

    add trigger db

--- init_db/triggers.sql
+++ init_db/triggers.sql
@@ -1,47 +1,47 @@
--- Trigger cho NAVIGATION_MENU_POSITION (trước khi INSERT)
 DELIMITER $$
-CREATE TRIGGER enforce_navbar_horizontal_before_insert 
-BEFORE INSERT ON cms
-FOR EACH ROW
-BEGIN
-  IF NEW.cms = 'NAVIGATION_MENU_POSITION' THEN
-    SET NEW.value = 'horizontal';
-  END IF;
-END$$
-DELIMITER ;
 
--- Trigger cho NAVIGATION_MENU_POSITION (trước khi UPDATE)
-DELIMITER $$
-CREATE TRIGGER enforce_navbar_horizontal_before_update 
-BEFORE UPDATE ON cms
+-- Trigger cho GLOBAL_THEME_NAME: ép giá trị luôn là 'default'
+CREATE TRIGGER enforce_global_theme_default_before_insert
+BEFORE INSERT ON setting
 FOR EACH ROW
 BEGIN
-  IF NEW.cms = 'NAVIGATION_MENU_POSITION' THEN
-    SET NEW.value = 'horizontal';
-  END IF;
-END$$
-DELIMITER ;
-
--- Trigger cho GLOBAL_THEME_NAME (trước khi INSERT)
-DELIMITER $$
-CREATE TRIGGER enforce_global_theme_default_before_insert 
-BEFORE INSERT ON cms
-FOR EACH ROW
-BEGIN
-  IF NEW.cms = 'GLOBAL_THEME_NAME' THEN
+  IF NEW.setting = 'GLOBAL_THEME_NAME' THEN
     SET NEW.value = 'default';
   END IF;
 END$$
-DELIMITER ;
 
--- Trigger cho GLOBAL_THEME_NAME (trước khi UPDATE)
 DELIMITER $$
-CREATE TRIGGER enforce_global_theme_default_before_update 
-BEFORE UPDATE ON cms
+
+CREATE TRIGGER enforce_global_theme_default_before_update
+BEFORE UPDATE ON setting
 FOR EACH ROW
 BEGIN
-  IF NEW.cms = 'GLOBAL_THEME_NAME' THEN
+  IF NEW.setting = 'GLOBAL_THEME_NAME' THEN
     SET NEW.value = 'default';
   END IF;
 END$$
+
+DELIMITER $$
+
+-- Trigger cho NAVIGATION_MENU_POSITION: ép giá trị luôn là 'horizontal'
+CREATE TRIGGER enforce_navbar_horizontal_before_insert
+BEFORE INSERT ON setting
+FOR EACH ROW
+BEGIN
+  IF NEW.setting = 'NAVIGATION_MENU_POSITION' THEN
+    SET NEW.value = 'horizontal';
+  END IF;
+END$$
+
+DELIMITER $$
+
+CREATE TRIGGER enforce_navbar_horizontal_before_update
+BEFORE UPDATE ON setting
+FOR EACH ROW
+BEGIN
+  IF NEW.setting = 'NAVIGATION_MENU_POSITION' THEN
+    SET NEW.value = 'horizontal';
+  END IF;
+END$$
+
 DELIMITER ;
