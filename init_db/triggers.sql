c096e52b5239e8da7703152cac8fe0b5c67dd77f
Lam Devops <lamdt@icsp.vn>
2025-03-10 16:17:09 +0700

    add trigger db

--- init_db/triggers.sql
+++ init_db/triggers.sql
@@ -1,10 +1,10 @@
 -- Trigger cho NAVIGATION_MENU_POSITION (trước khi INSERT)
 DELIMITER $$
 CREATE TRIGGER enforce_navbar_horizontal_before_insert 
-BEFORE INSERT ON setting
+BEFORE INSERT ON cms
 FOR EACH ROW
 BEGIN
-  IF NEW.setting = 'NAVIGATION_MENU_POSITION' THEN
+  IF NEW.cms = 'NAVIGATION_MENU_POSITION' THEN
     SET NEW.value = 'horizontal';
   END IF;
 END$$
@@ -13,10 +13,10 @@ DELIMITER ;
 -- Trigger cho NAVIGATION_MENU_POSITION (trước khi UPDATE)
 DELIMITER $$
 CREATE TRIGGER enforce_navbar_horizontal_before_update 
-BEFORE UPDATE ON setting
+BEFORE UPDATE ON cms
 FOR EACH ROW
 BEGIN
-  IF NEW.setting = 'NAVIGATION_MENU_POSITION' THEN
+  IF NEW.cms = 'NAVIGATION_MENU_POSITION' THEN
     SET NEW.value = 'horizontal';
   END IF;
 END$$
@@ -25,10 +25,10 @@ DELIMITER ;
 -- Trigger cho GLOBAL_THEME_NAME (trước khi INSERT)
 DELIMITER $$
 CREATE TRIGGER enforce_global_theme_default_before_insert 
-BEFORE INSERT ON setting
+BEFORE INSERT ON cms
 FOR EACH ROW
 BEGIN
-  IF NEW.setting = 'GLOBAL_THEME_NAME' THEN
+  IF NEW.cms = 'GLOBAL_THEME_NAME' THEN
     SET NEW.value = 'default';
   END IF;
 END$$
@@ -37,10 +37,10 @@ DELIMITER ;
 -- Trigger cho GLOBAL_THEME_NAME (trước khi UPDATE)
 DELIMITER $$
 CREATE TRIGGER enforce_global_theme_default_before_update 
-BEFORE UPDATE ON setting
+BEFORE UPDATE ON cms
 FOR EACH ROW
 BEGIN
-  IF NEW.setting = 'GLOBAL_THEME_NAME' THEN
+  IF NEW.cms = 'GLOBAL_THEME_NAME' THEN
     SET NEW.value = 'default';
   END IF;
 END$$
