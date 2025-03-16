81d9ad794e86ac8f432d2458c1f6eae53f3e9eef
Lam Devops <lamdt@icsp.vn>
2025-03-16 15:33:09 +0700

    s

--- web/settings.php
+++ web/settings.php
@@ -17,11 +17,14 @@ global $dbname;
 global $dbssl;
 global $dbsslverify;
 
-$dbhost = $_SERVER['MYSQL_HOST'] . ':' . $_SERVER['MYSQL_PORT'];
-$dbuser = $_SERVER['MYSQL_USER'];
-$dbpass = $_SERVER['MYSQL_PASSWORD'];
-$dbname = $_SERVER['MYSQL_DATABASE'];
-$dbssl = $_SERVER['MYSQL_ATTR_SSL_CA'];
+$dbhost = isset($_SERVER['MYSQL_HOST']) ? $_SERVER['MYSQL_HOST'] : 'db';
+$dbport = isset($_SERVER['MYSQL_PORT']) ? $_SERVER['MYSQL_PORT'] : '3306';
+$dbuser = isset($_SERVER['MYSQL_USER']) ? $_SERVER['MYSQL_USER'] : 'root';
+$dbpass = isset($_SERVER['MYSQL_PASSWORD']) ? $_SERVER['MYSQL_PASSWORD'] : 'root';
+$dbname = isset($_SERVER['MYSQL_DATABASE']) ? $_SERVER['MYSQL_DATABASE'] : 'cms';
+
+$dbhost = $dbhost . ':' . $dbport;
+
 $dbsslverify = $_SERVER['MYSQL_ATTR_SSL_VERIFY_SERVER_CERT'];
 
 if (!defined('SECRET_KEY')) {
